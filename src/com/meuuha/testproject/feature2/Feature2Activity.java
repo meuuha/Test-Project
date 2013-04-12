6a9ba7fbe9ea49a1f3c608d447b2f3a09fcd482b
Youssef AMRANI <yamrani@neotilus-w0009.oceanie.intranet.degetel.com>
2013-04-12 15:43:06 +0200

    implement feature 2 button

--- src/com/meuuha/testproject/feature2/Feature2Activity.java
+++ src/com/meuuha/testproject/feature2/Feature2Activity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 import com.meuuha.testproject.R;
 
@@ -14,6 +15,8 @@ public class Feature2Activity extends Activity implements OnClickListener{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.feature2);
+		
+		findViewById(R.id.feature2_button1).setOnClickListener(this);
 	}
 
 	@Override
@@ -25,6 +28,11 @@ public class Feature2Activity extends Activity implements OnClickListener{
 
 	@Override
 	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.feature2_button1:
+			Toast.makeText(this, "feature 2 - button click 1", Toast.LENGTH_SHORT).show();
+			break;
+		}
 	}
 
 }
